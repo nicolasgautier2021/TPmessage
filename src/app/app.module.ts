d3cb902f2746767c4deb8a459d0d675542b82c2f
Nicolas Gautier <nicolas.gautier2021@campus-eni.fr>
2021-09-16 11:56:31 +0200

    Nouveau dépôt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeteComponent } from './tete/tete.component';
+import { GaucheComponent } from './gauche/gauche.component';
+import { BasComponent } from './bas/bas.component';
+import { HautComponent } from './haut/haut.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeteComponent,
+    GaucheComponent,
+    BasComponent,
+    HautComponent
   ],
   imports: [
     BrowserModule
